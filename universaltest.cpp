b28fb84d05ac050d54fade620010a1d3018bf773
Partical777 <z25993021@gmail.com>
2016-12-03 23:57:14 +0800

    增加隨機骰子function
    只增加了骰子
    好不容易想到個幾乎隨機的取數方法

--- universaltest.cpp
+++ universaltest.cpp
@@ -2,8 +2,14 @@
 #include <iostream>
 #include <algorithm>
 #include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
+
+int dice();
+int result1,result2;
 
 struct  Player{
 
@@ -17,10 +23,10 @@ struct  Player{
 //---------------------------------------main----------------------------------------------------------------------------------
 int main(){
      struct Player play[] = {
-       {500,100,0,0,},  //player1  use play[0]
-       {500,100,0,0,},  //player2  use play[1]
-       {500,100,0,0,},  //player3  use play[2]
-       {500,100,0,0,},  //player4  use play[3]
+       {500,100,0,0},  //player1  use play[0]
+       {500,100,0,0},  //player2  use play[1]
+       {500,100,0,0},  //player3  use play[2]
+       {500,100,0,0},  //player4  use play[3]
      };
     for(int k=0;k<4;k++){
        for(int i=0;i<3;i++){
@@ -30,8 +36,34 @@ int main(){
                            }
                         }    // set up to 0
 
+     int DiceNumber;
+
+     int n[3]={1,2,3};
       // -----------------------up it's basic set up -------------------------------------------------------------------
 
-     cout<<play[0].energy<<play[0].x<<play[0].y<<play[0].position[0][0]<<endl;
+      cout<<"請輸入任意個位數擲出第1顆骰子"<<endl;
+      cin>>DiceNumber;
+      result1=dice();
+      cout<<"第一顆骰子為"<<result1<<endl;
 
+      cout<<"請再輸入任意個位數擲出第2顆骰子"<<endl;
+      cin>>DiceNumber;
+      result2=dice();
+      cout<<"第二顆骰子為"<<result2<<endl;
+
+     cout<<play[0].energy<<play[0].x<<play[0].y<<play[0].position[0][0]<<result1<<result2<<endl;
+
+}
+
+int dice(){
+    //  骰子程序
+    int result;
+    srand(time(NULL));
+     int random[10000];
+    for(int i=0;i<10000;i++)
+    {
+        random[i]={(rand()%3)+1};
+    }
+    result = random[rand()%10000];
+   return result;
 }
